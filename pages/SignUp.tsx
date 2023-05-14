65cfd09cf51c1f8544204b8f63210c32f588ea54
Aman kumar <amankumar02740@gmail.com>
2023-05-14 22:38:10 +0530

    added some more ui

--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -98,13 +98,14 @@ function Signup() {
       <body
         className="bg-gray-100">
         <div className="mb-5">
-          <h1 className='text-black text-6xl flex  justify-center'>ashberri</h1><br />
-          <p className='text-gray-600 text-2xl flex justify-center '>People those how highly compatible  <br /> and  think  like U,connect in ashberri...</p>
+          <h1 className='black text-5xl flex  justify-center pt-7 phone:text-1xl'>ashberri</h1><br />
+
+          {/* <p className='text-gray-600 text-2xl flex justify-center '>People those how highly compatible  <br /> and  think  like U,connect in ashberri...</p> */}
         </div>
         <div
           className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
         >
-          <div className="relative w-auto my-6 ">
+          <div className="relative w-96 my-6 ">
             {/*content*/}
             <div className="border-0 rounded-lg shadow-lg relative flex flex-col  bg-white outline-none focus:outline-none">
               {/*header*/}
