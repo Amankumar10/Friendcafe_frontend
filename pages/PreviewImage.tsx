aec70e014219e7dce8c3e3a4cdb2d35cdd843fa2
Aman kumar <amankumar02740@gmail.com>
2023-05-25 20:58:25 +0530

    added update file and all field using rtk query

--- pages/PreviewImage.tsx
+++ pages/PreviewImage.tsx
@@ -1,19 +1,28 @@
+// import React from 'react'
+
+// const PreviewImage = ({file}) => {
+//     const [preview,setPreview] = React.useState(null);
+//     const reader = new FileReader()
+//     reader.readAsDataURL(file)
+//     reader.onload = () =>{
+//        setPreview(reader.result); 
+//     }
+//   return (
+//     <div>
+//         <img src={preview} alt="preview" width="200px" height="200px"/>
+//     </div>
+//   )
+// }
+
+// export default PreviewImage
+
+
 import React from 'react'
 
-const PreviewImage = ({file}) => {
-    const [preview,setPreview] = React.useState(null);
-    const reader = new FileReader()
-    reader.readAsDataURL(file)
-    reader.onload = () =>{
-       setPreview(reader.result); 
-    }
+const PreviewImage = () => {
   return (
-    <div>
-        <img src={preview} alt="preview" width="200px" height="200px"/>
-    </div>
+    <div>PreviewImage</div>
   )
 }
 
 export default PreviewImage
-
-
